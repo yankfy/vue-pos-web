051f343f12e46f14e49e735ccc31394a9bcad4d3
yankfy <yankfy@qq.com>
2017-09-26 00:55:23 +0800

    demo版初始化页面

---
+++ src/components/Pos.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="pos">
+        <p>{{msg}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"pos",
+    data:() =>{
+        return {
+            msg:"超棒的收银系统",
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Pos from '@/components/Pos'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:"history",
   routes: [
     {
       path: '/',
-      name: 'Hello',
+      name: 'pos',
+      component: Pos
+    },
+    {
+      path: '/hello',
+      name: 'hello',
       component: Hello
     }
+    // {
+    //   path: '*',
+    //   name: 'hello',
+    //   component: Hello
+    // }
   ]
 })
